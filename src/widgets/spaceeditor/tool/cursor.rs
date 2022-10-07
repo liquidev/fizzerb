45a9b3a7c0667db70254e5879ee4fe05de40c598
lqdev <liquidev@tutanota.com>
2022-10-07 22:38:56 +0200

    dragging objects

--- src/widgets/spaceeditor/tool/cursor.rs
+++ src/widgets/spaceeditor/tool/cursor.rs
@@ -1,7 +1,11 @@
 use druid::{
     kurbo::{Circle, Line},
     piet::{LineCap, StrokeStyle},
-    Env, Event, EventCtx, PaintCtx, Point, RenderContext,
+    Env, Event, EventCtx, PaintCtx, Point, RenderContext, Vec2,
+};
+use spaceeditor::{
+    data::{Microphone, Speaker, Wall},
+    transform::Transform,
 };
 
 use super::ToolImpl;
@@ -13,9 +17,17 @@ use crate::{
     },
 };
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum State {
+    Idle,
+    DraggingEntireFocusedObject,
+}
+
 pub struct CursorTool {
     hot_object: Option<usize>,
     focused_object: Option<usize>,
+    state: State,
+    last_mouse_pos: Point,
 }
 
 impl CursorTool {
@@ -23,6 +35,8 @@ impl CursorTool {
         Self {
             hot_object: None,
             focused_object: None,
+            state: State::Idle,
+            last_mouse_pos: Point::ZERO,
         }
     }
 
@@ -63,6 +77,17 @@ impl CursorTool {
             }
         }
     }
+
+    fn drag_entire_object(&mut self, object: &mut Object, delta: Vec2) {
+        match object {
+            Object::Wall(Wall { start, end, .. }) => {
+                *start += delta;
+                *end += delta;
+            }
+            Object::Microphone(Microphone { position })
+            | Object::Speaker(Speaker { position, .. }) => *position += delta,
+        }
+    }
 }
 
 impl ToolImpl for CursorTool {
@@ -73,60 +98,116 @@ impl ToolImpl for CursorTool {
         data: &mut SpaceEditorProjectData,
         env: &Env,
     ) {
-        if let Event::MouseMove(event) = event {
-            let object_params = CachedObjectParams::from_env(env);
-            let previous_hot = self.hot_object;
-            self.make_object_hot_at_position(&object_params, &data.space, event.pos);
-            if self.hot_object != previous_hot {
+        match (self.state, event) {
+            (State::Idle, Event::MouseMove(mouse)) => {
+                let object_params = CachedObjectParams::from_env(env);
+                let previous_hot = self.hot_object;
+                self.make_object_hot_at_position(&object_params, &data.space, mouse.pos);
+                if self.hot_object != previous_hot {
+                    ctx.request_paint();
+                }
+            }
+            (State::Idle, Event::MouseDown(_)) => {
+                self.focused_object = self.hot_object;
+                if self.hot_object.is_some() {
+                    self.state = State::DraggingEntireFocusedObject;
+                    ctx.request_paint();
+                }
+            }
+
+            (State::DraggingEntireFocusedObject, Event::MouseMove(mouse)) => {
+                if let Some(focused_object) = self.focused_object {
+                    let object = &mut data.edit_space().objects[focused_object];
+                    let delta = mouse.pos - self.last_mouse_pos;
+                    self.drag_entire_object(object, delta);
+                    ctx.request_paint();
+                }
+            }
+
+            (_, Event::MouseUp(_)) => {
+                self.state = State::Idle;
                 ctx.request_paint();
             }
+
+            _ => (),
+        }
+
+        if let Event::MouseMove(mouse) = event {
+            self.last_mouse_pos = mouse.pos;
         }
     }
 
     fn paint(&mut self, ctx: &mut PaintCtx, data: &SpaceEditorProjectData, env: &Env) {
-        let primary_color = env.get(style::PRIMARY_SELECTION_COLOR);
         let viewport_size = ctx.size();
 
+        if let Some(focused_index) = self.focused_object {
+            let object = &data.space.objects[focused_index];
+            paint_object_outline(
+                ctx,
+                env,
+                &data.transform,
+                viewport_size,
+                object,
+                env.get(style::FOCUSED_OUTLINE_THICKNESS),
+            );
+        }
+
         if let Some(hot_index) = self.hot_object {
-            let object = &data.space.objects[hot_index];
-            let thickness = env.get(style::HOT_OUTLINE_THICKNESS);
-            let stroke_style = StrokeStyle::default().line_cap(LineCap::Round);
-            match object {
-                Object::Wall(wall) => {
-                    let start = data.transform.to_screen_space(wall.start, viewport_size);
-                    let end = data.transform.to_screen_space(wall.end, viewport_size);
-                    ctx.stroke_styled(
-                        Line::new(start, end),
-                        &primary_color,
-                        thickness,
-                        &stroke_style,
-                    );
-                }
-                Object::Microphone(microphone) => {
-                    let position = data
-                        .transform
-                        .to_screen_space(microphone.position, viewport_size);
-                    let radius = env.get(style::MICROPHONE_RADIUS) * data.transform.zoom();
-                    ctx.stroke_styled(
-                        Circle::new(position, radius),
-                        &primary_color,
-                        thickness,
-                        &stroke_style,
-                    );
-                }
-                Object::Speaker(speaker) => {
-                    let position = data
-                        .transform
-                        .to_screen_space(speaker.position, viewport_size);
-                    let radius = env.get(style::SPEAKER_RADIUS) * data.transform.zoom();
-                    ctx.stroke_styled(
-                        Circle::new(position, radius),
-                        &primary_color,
-                        thickness,
-                        &stroke_style,
-                    );
-                }
+            if self.hot_object != self.focused_object {
+                let object = &data.space.objects[hot_index];
+                paint_object_outline(
+                    ctx,
+                    env,
+                    &data.transform,
+                    viewport_size,
+                    object,
+                    env.get(style::HOT_OUTLINE_THICKNESS),
+                );
             }
+        }
+    }
+}
+
+fn paint_object_outline(
+    ctx: &mut PaintCtx,
+    env: &Env,
+    transform: &Transform,
+    viewport_size: druid::Size,
+    object: &Object,
+    thickness: f64,
+) {
+    let primary_color = env.get(style::PRIMARY_SELECTION_COLOR);
+    let stroke_style = StrokeStyle::default().line_cap(LineCap::Round);
+    match object {
+        Object::Wall(wall) => {
+            let start = transform.to_screen_space(wall.start, viewport_size);
+            let end = transform.to_screen_space(wall.end, viewport_size);
+            ctx.stroke_styled(
+                Line::new(start, end),
+                &primary_color,
+                thickness,
+                &stroke_style,
+            );
+        }
+        Object::Microphone(microphone) => {
+            let position = transform.to_screen_space(microphone.position, viewport_size);
+            let radius = env.get(style::MICROPHONE_RADIUS) * transform.zoom();
+            ctx.stroke_styled(
+                Circle::new(position, radius),
+                &primary_color,
+                thickness,
+                &stroke_style,
+            );
+        }
+        Object::Speaker(speaker) => {
+            let position = transform.to_screen_space(speaker.position, viewport_size);
+            let radius = env.get(style::SPEAKER_RADIUS) * transform.zoom();
+            ctx.stroke_styled(
+                Circle::new(position, radius),
+                &primary_color,
+                thickness,
+                &stroke_style,
+            );
         }
     }
 }
